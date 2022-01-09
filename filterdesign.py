14ec326b447dcba12ed83e2ccd754f361497344b
Bruce MacKinnon <bruce@mackinnon.com>
2022-01-09 15:52:57 -0500

    Added abilty to create normalized components

--- filterdesign.py
+++ filterdesign.py
@@ -1,6 +1,34 @@
 # Functions related to filter design
 import math 
 
+def butterworthNormalizedComponents(n):
+    """ Creates the g(k) components of a standard Butterworth low-pass filter. """
+    g_list = []
+    for k in range(1, n+1):
+        g_list.append(2.0 * math.sin((2.0 * k - 1) * math.pi / (2.0 * n)))
+    return g_list
+
+def chebyshevNormalizedComponents(n, ripple_db):
+    """ Creates the g(k) components of a standard Chebyshev low-pass filter. """
+    g_list = []
+    d = ripple_db / 8.68589
+    B = (1.0 / (2.0 * n)) * math.log((math.exp(d) + 1) / (math.exp(d) - 1))
+    B0 = 2 * n * B
+    N = (1.0 / 2.0) * (math.exp(B) - math.exp(-B))
+    for k in range(1, n+1):
+        ak = math.sin(((2.0 * k - 1.0) * math.pi ) / (2.0 * n))
+        bk = N ** 2.0 + math.sin(k * math.pi / n) ** 2.0
+        if k == 1:
+            gk = 2.0 * ak / N
+        else:
+            gk = (4 * ak_minus_1 * ak) / (bk_minus_1 * gk_minus_1)
+        g_list.append(gk)
+        # Keep this stuff for the next iteration
+        ak_minus_1 = ak
+        bk_minus_1 = bk
+        gk_minus_1 = gk
+    return g_list
+
 def cutoffAdjustmentChebyshev(ripple_db, n):
     """ Creates the w coefficient that adjusts the filter for a -3dB cutoff (as opposed to the 
         normal "maximal ripple" cutoff that would come from the standard parameters).
@@ -10,6 +38,13 @@ def cutoffAdjustmentChebyshev(ripple_db, n):
     w = (1.0 / 2.0) * (math.exp(y) + math.exp(-y))
     return w
 
+def couplingCoefficientsButterworth(g_params):
+    """  Creates the knm coupling coefficients  given a set of g parameters from the
+         standard low-pass polynomials.  Expect to get n-1 coupling coefficients 
+         back.
+    """
+    return _couplingCoefficients(1.0, g_params)
+
 def couplingCoefficientsChebyshev(ripple_db, g_params):
     """  Creates the knm coupling coefficients  given a set of g parameters from the
          standard low-pass polynomials.  Expect to get n-1 coupling coefficients 
@@ -17,13 +52,21 @@ def couplingCoefficientsChebyshev(ripple_db, g_params):
     """
     n = len(g_params)
     w = cutoffAdjustmentChebyshev(ripple_db, n)
+    return _couplingCoefficients(w, g_params)
+
+def _couplingCoefficients(w, g_params):
+    """  Creates the knm coupling coefficients  given a set of g parameters from the
+         standard low-pass polynomials.  Expect to get n-1 coupling coefficients 
+         back.
+    """
+    n = len(g_params)
     k_list = []
     for i in range(0, n-1):
         k = (1.0 / w) * (1.0 / ( math.sqrt(g_params[i] * g_params[i+1]) ))
         k_list.append(k)
     return k_list
 
-def endSectionCoefficientChebyshev(ripple_db, g_params, rs_ohms):
+def endSectionCoefficientChebyshev(ripple_db, g_params):
     """  Creates the q1/qn end coefficient given a set of g parameters from the
          standard low-pass polynomials.  We are assuming symmetry here.
     """
@@ -32,5 +75,11 @@ def endSectionCoefficientChebyshev(ripple_db, g_params, rs_ohms):
     if n % 2 == 0:
         raise Exception("Enven order Chebyshev not supported")
     w = cutoffAdjustmentChebyshev(ripple_db, n)
-    return g_params[0] * w / rs_ohms
+    return g_params[0] * w
+
+def endSectionCoefficientButterworth(g_params):
+    """  Creates the q1/qn end coefficient given a set of g parameters from the
+         standard low-pass polynomials.  We are assuming symmetry here.
+    """
+    return g_params[0]
 
--- unittest-1.py
+++ unittest-1.py
@@ -3,19 +3,48 @@ from filterdesign import *
 
 class TestFilters(unittest.TestCase):
 
+    def assertListAlmostEqual(self, l0, l1, places):
+        self.assertEqual(len(l0), len(l1))
+        for k in range(0, len(l0)):
+            self.assertAlmostEqual(l0[k], l1[k], places=places)
+
+    def test_0(self):
+        # Creating the Butterworth coefficients
+        # Taken from ARRL Handbook 2016 pg. 11.12
+        expected_g_list = [0.7654, 1.848, 1.848, 0.7654 ]
+        g_list = butterworthNormalizedComponents(4)
+        self.assertListAlmostEqual(expected_g_list, g_list, 3)
+
+        # Creating Chebyshev components
+        # Taken from IRFD pg. 65
+        expected_g_list = [ 0.8430, 0.6220 ]
+        g_list = chebyshevNormalizedComponents(2, 0.1)
+        self.assertListAlmostEqual(expected_g_list, g_list, 3)
+
+        expected_g_list = [ 1.0316, 1.1474, 1.0316 ]
+        g_list = chebyshevNormalizedComponents(3, 0.1)
+        self.assertListAlmostEqual(expected_g_list, g_list, 3)
+
     def test_1(self):
         # Taken from Hayward "Designing Narrow-Badwith Ladder Filters" pg. 40
         self.assertAlmostEqual(1.1347, cutoffAdjustmentChebyshev(0.1, 5), places=4)
 
         # Taken from Hayward "Designing Narrow-Badwith Ladder Filters" pg. 41
         # NOTE: We are only using the first two g parameters here
-        g_list = [ 1.1468, 1.3712, 1, 1, 1 ]
+        g_list = chebyshevNormalizedComponents(5, 0.1)
         self.assertAlmostEqual(0.7028, couplingCoefficientsChebyshev(0.1, g_list)[0], places=4)
  
         # Taken from Hayward "Designing Narrow-Badwith Ladder Filters" pg. 41
         # NOTE: We are only using the first g parameter here
-        g_list = [ 1.1468, 1, 1, 1, 1 ]
-        self.assertAlmostEqual(1.3013, endSectionCoefficientChebyshev(0.1, g_list, 1.0), places=4)
+        g_list = chebyshevNormalizedComponents(5, 0.1)
+        self.assertAlmostEqual(1.3013, endSectionCoefficientChebyshev(0.1, g_list), places=4)
+
+        # Taken from Hayward "Designing Narrow-Badwith Ladder Filters" pg. 5
+        g_list = butterworthNormalizedComponents(4)
+        expected_k_list = [ 0.841, 0.541, 0.841 ]
+        k_list = couplingCoefficientsButterworth(g_list)
+        self.assertListAlmostEqual(expected_k_list, k_list, 3)
+        self.assertAlmostEqual(0.7654, endSectionCoefficientButterworth(g_list), places=4)
 
 
 if __name__ == '__main__':
