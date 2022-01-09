3503706ac2fdb7aa75e96f954ea408dfafdadc60
Bruce MacKinnon <bruce@mackinnon.com>
2022-01-09 14:17:28 -0500

    Added end section

--- filterdesign.py
+++ filterdesign.py
@@ -1,20 +1,36 @@
 # Functions related to filter design
 import math 
 
-def cutoffAdjustmentChebyshev(rippleDb, n):
-    """ Gets the w coefficient that adjusts the filter for a -3dB cutoff (as opposed to the 
+def cutoffAdjustmentChebyshev(ripple_db, n):
+    """ Creates the w coefficient that adjusts the filter for a -3dB cutoff (as opposed to the 
         normal "maximal ripple" cutoff that would come from the standard parameters).
     """
-    e = math.sqrt(10 ** (rippleDb / 10) - 1) 
+    e = math.sqrt(10 ** (ripple_db / 10) - 1) 
     y = (1.0 / n) * math.log(1/e + math.sqrt(1.0 / e**2 - 1.0))
     w = (1.0 / 2.0) * (math.exp(y) + math.exp(-y))
     return w
 
-def couplingCoefficientsChebyshev(rippleDb, g_params):
+def couplingCoefficientsChebyshev(ripple_db, g_params):
+    """  Creates the knm coupling coefficients  given a set of g parameters from the
+         standard low-pass polynomials.  Expect to get n-1 coupling coefficients 
+         back.
+    """
     n = len(g_params)
-    w = cutoffAdjustmentChebyshev(rippleDb, n)
+    w = cutoffAdjustmentChebyshev(ripple_db, n)
     k_list = []
     for i in range(0, n-1):
         k = (1.0 / w) * (1.0 / ( math.sqrt(g_params[i] * g_params[i+1]) ))
         k_list.append(k)
     return k_list
+
+def endSectionCoefficientChebyshev(ripple_db, g_params, rs_ohms):
+    """  Creates the q1/qn end coefficient given a set of g parameters from the
+         standard low-pass polynomials.  We are assuming symmetry here.
+    """
+    n = len(g_params)
+    # Enforce oddness
+    if n % 2 == 0:
+        raise Exception("Enven order Chebyshev not supported")
+    w = cutoffAdjustmentChebyshev(ripple_db, n)
+    return g_params[0] * w / rs_ohms
+
--- unittest-1.py
+++ unittest-1.py
@@ -4,13 +4,19 @@ from filterdesign import *
 class TestFilters(unittest.TestCase):
 
     def test_1(self):
-        # Take from Hayward "Designing Narrow-Badwith Ladder Filters" pg. 40
+        # Taken from Hayward "Designing Narrow-Badwith Ladder Filters" pg. 40
         self.assertAlmostEqual(1.1347, cutoffAdjustmentChebyshev(0.1, 5), places=4)
 
-        # Take from Hayward "Designing Narrow-Badwith Ladder Filters" pg. 41
+        # Taken from Hayward "Designing Narrow-Badwith Ladder Filters" pg. 41
         # NOTE: We are only using the first two g parameters here
         g_list = [ 1.1468, 1.3712, 1, 1, 1 ]
         self.assertAlmostEqual(0.7028, couplingCoefficientsChebyshev(0.1, g_list)[0], places=4)
  
+        # Taken from Hayward "Designing Narrow-Badwith Ladder Filters" pg. 41
+        # NOTE: We are only using the first g parameter here
+        g_list = [ 1.1468, 1, 1, 1, 1 ]
+        self.assertAlmostEqual(1.3013, endSectionCoefficientChebyshev(0.1, g_list, 1.0), places=4)
+
+
 if __name__ == '__main__':
     unittest.main()
